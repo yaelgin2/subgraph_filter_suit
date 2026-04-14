a2f8631c3b367b1fcd4d9faed3f265600cb29995
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-04-14 21:39:40 +0300

    Inheritance logging solution changed to wrapper class

--- include/logging/LoggerHandler.h
+++ include/logging/LoggerHandler.h
@@ -8,23 +8,22 @@
 namespace sgf
 {
 
-class Loggable
+class LoggerHandler
 {
-protected:
+private:
     std::weak_ptr<ILogger> logger;
+
+public:
+    explicit LoggerRef(std::weak_ptr<ILogger> log)
+        : logger(std::move(log))
+    {}
 
     void log(LogLevel level, const std::string& msg) const
     {
-        if (std::shared_ptr<ILogger> ptr = logger.lock())
+        if (auto ptr = logger.lock())
         {
             ptr->log(level, msg);
         }
-    }
-
-public:
-    explicit Loggable(std::weak_ptr<ILogger> log)
-        : logger(std::move(log))
-    {
     }
 };
 
