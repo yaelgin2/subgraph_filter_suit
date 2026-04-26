d515b322cdd44b48383702151c591f4f7f6a39ca
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-04-26 19:53:52 +0300

    Implemented enumerator

--- src/preprocessing/GroupEnumerationPreprocessor.cpp
+++ src/preprocessing/GroupEnumerationPreprocessor.cpp
@@ -15,34 +15,62 @@ GroupEnmerationPreprocessor::GroupEnmerationPreprocessor(std::shared_ptr<Colored
 std::unordered_map<__uint128_t, uint32_t> GroupEnmerationPreprocessor::calculate()
 {
     m_logger.log(LogLevel::INFO, "Starting graph enumeration calculation.");
+    std::unordered_map<__uint128_t, uint32_t> motif_count;
+    std::vector<std::vector<bool>> adjacency_matrix;
+    graph_to_adjacency_matrix(adjacency_matrix);
+    sort_nodes();
+    Groups groups = find_groups();
+    uint32_t groups_iterated_overs = 0;
+    for (const auto& [motif_num, group] : groups)
+    {
+        vecotr<std::vector<bool>> group_adjacency_matrix = group_to_adjacency_matrix(group);
+        motif_count[calculate_motif_number(motif_num, group_adjacency_matrix)] += 1;
+        if (groups_iterated_overs % 1000 == 0 && VERBOSE)
+        {
+            m_logger.log(LogLevel::DEBUG, "Found groups: " + std::to_string(groups_iterated_overs));
+        }
+        groups_iterated_overs++;
+    }
+    m_logger.log(LogLevel::INFO, "Finished graph enumeration calculation.");
+    return motif_count;
+}
 
-        self._features = {}
-        if self.calc_nodes:
-            self._features = {node: {} for node in self._graph}
-        elif self.calc_edges:
-            self._features = {edge: {} for edge in self._graph.edges()}
-        if self.count_motifs:
-            self.features[self.MOTIF_SUM_KEY] = {}
+void GroupEnmerationPreprocessor::graph_to_adjacency_matrix(std::vector<std::vector<bool>>& adjacency_matrix)
+{
+    adjacency_matrix.resize(m_graph->num_vertices());
+    for (size_t i = 0; i < m_graph->num_vertices(); i++)
+    {
+        adjacency_matrix[i].resize(m_graph->num_vertices(), false);
+    }
+    for (int node = 0; node < m_graph->num_vertices(); node++)
+    {
+        auto [neighbour_begin, neighbour_end] = m_graph->get_neighbours(node);
+        for (auto neighbour_iterator = neighbour_begin; neighbour_iterator != neighbour_end; neighbour_iterator++)
+        {
+            adjacency_matrix[node][*neighbour_iterator] = true;
+        }
+    }
+}
 
-        for i, (group, motif_num) in enumerate(self._calculate_motif()):
-            if self.calc_nodes:
-                self._update_nodes_group(group, motif_num)
-            elif self.calc_edges:
-                self._update_edges(group, motif_num)
-            if self.count_motifs:
-                if motif_num not in self._features[self.MOTIF_SUM_KEY]:
-                    self._features[self.MOTIF_SUM_KEY][motif_num] = 0
-                self._features[self.MOTIF_SUM_KEY][motif_num] += 1
-
-            if (i + 1) % 1000 == 0 and VERBOSE:
-                self._logger.debug("Groups: %d" % i)
-
-        self._logger.info(f"Finished calculating {self._level} motifs.")
-
-        if not self.count_motifs:
-#clean features
-            self._all_relevant_motifs = {motif_number for node_motifs in self._features.values()
-                                         for motif_number, appearances in node_motifs.items()}
+std::vector<std::vector<bool>>> group_to_adjacency_matrix(const std::vector<uint32_t>& group)
+{
+    std::vector<std::vector<bool>> group_adjacency_matrix;
+    group_adjacency_matrix.resize(group.size());
+    for (size_t vertex_index = 0; vertex_index < group.size(); vertex_index++)
+    {
+        group_adjacency_matrix[vertex_index].resize(group.size(), false);
+    }
+    for (size_t vertex_index = 0; vertex_index < group.size(); vertex_index++)
+    {
+        for(size_t neighbour_index = 0; neighbour_index < group.size(); neighbour_index++)
+        {
+            if (m_graph->is_edge(group[vertex_index], group[neighbour_index]))
+            {
+                group_adjacency_matrix[vertex_index][neighbour_index] = true;
+            }
+        }
+    }
+    return group_adjacency_matrix;
 }
 
 }  // namespace sgf
