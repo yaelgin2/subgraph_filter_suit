40f67b5aa934bed1e20ae9c1045e90b459f46aa4
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-04-26 17:22:33 +0300

    Fixed documintation

--- include/Constants.h
+++ include/Constants.h
@@ -9,7 +9,7 @@ namespace sgf
 class SgfConstants
 {
 public:
-    /// Maximum vertex color that can be stored in ColoredGraph's int32_t array.
+    /// Maximum vertex color that can be stored in ColoredGraph's uint32_t array.
     static constexpr uint32_t MAX_VERTEX_COLOR = static_cast<uint32_t>((1 << 24) - 1);
 };
 
