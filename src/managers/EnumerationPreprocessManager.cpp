64dd78d7d15348f717e8f8ca57fab0f91c8b5a9e
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-05-12 19:42:47 +0300

    fixed includes and type in loop

--- src/managers/EnumerationPreprocessManager.cpp
+++ src/managers/EnumerationPreprocessManager.cpp
@@ -1,5 +1,9 @@
 #include "EnumerationPreprocessManager.h"
 
+#include "LogLevel.h"
+
+#include <string>
+
 namespace sgf
 {
 
@@ -15,9 +19,9 @@ EnumerationPreprocessManager::preprocess(const PreprocessorFactory& factory) con
 {
     std::vector<std::unordered_map<UInt128, uint32_t, UInt128Hash>> results;
     results.reserve(m_library.size());
-    for (uint32_t = graph_index = 0, graph_index < m_library; ++graph_index)
+    for (size_t graph_index = 0U; graph_index < m_library.size(); ++graph_index)
     {
-        const ColoredGraph& graph = m_library[graph_index]
+        const ColoredGraph& graph = m_library[graph_index];
         m_logger.log(LogLevel::INFO, "Processing graph " + std::to_string(graph_index));
         std::unique_ptr<IGraphPreprocessor> preprocessor = factory(graph, m_logger);
         results.push_back(preprocessor->calculate());
