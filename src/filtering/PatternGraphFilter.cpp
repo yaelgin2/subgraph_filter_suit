4c0a84ceb4e4e81f6894a0e8168582e5328bf14f
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-05-26 22:47:10 +0300

    Filter v1

--- src/filtering/PatternGraphFilter.cpp
+++ src/filtering/PatternGraphFilter.cpp
@@ -10,9 +10,39 @@ PatternGraphFilter::PatternGraphFilter(std::vector<PatternPreprocessorResult> li
 {
 }
 
-FilterResult PatternGraphFilter::filter(const ColoredGraph& graph) const
+bool is_check_relevant(const FilterResult& result, const std::unordered_set<uint32_t>& indices_to_filter)
 {
-    // TODO
+    return std::all_of(indices_to_filter.cbegin(), indices_to_filter.cend(),
+                       [&result](const uint32_t index) { return result[index]; });
 }
+
+FilterResult PatternGraphFilter::filter(const ColoredGraph& graph, bool is_induced, PriorPolicy prior_policy) const
+{
+    FilterResult result(m_library_cache.size(), false);
+    for (size_t library_index = 0; library_index < m_library_cache.size(); ++library_index)
+    {
+        
+        const PatternPreprocessorResult& library_result = m_library_cache[library_index];
+        SubgraphSearcher pattern_searcher(prior_policy, graph.is_directed(), is_induced);
+        for (const auto& [motif_descriptor, query_count] : query_result.m_motif_counts)
+        {
+            if (is_check_relevant(result, m_library_cache[library_index].second))
+            {
+                if(pattern_searcher.find_all(graph, m_library_cache[library_index].first) < query_count)
+                {
+                    for (const uint32_t index : m_library_cache[library_index].second)
+                    {
+                        if (!result[index])
+                        {
+                            m_logger.log(LogLevel::INFO,
+                                 "Pruned library graph " + std::to_string(index));
+                        }
+                        result[index] = true;
+                    }
+                }
+            }
+        }
+    }
+    return result;
 
 }  // namespace sgf
