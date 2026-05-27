d8c613661a7b54f3bc983eb95fa6794bb5fa6449
Yael Ginzburg <ginzburg.yael@gmail.com>
2026-05-27 23:13:57 +0300

    tidied

--- src/filtering/PatternGraphFilter.cpp
+++ src/filtering/PatternGraphFilter.cpp
@@ -1,7 +1,18 @@
 #include "PatternGraphFilter.h"
 
-#include "SubgraphSearcher.h"
+#include "ColoredGraph.h"
+#include "FilteringUtils.h"
+#include "LogLevel.h"
+#include "LoggerHandler.h"
 #include "PriorPolicy.h"
+#include "SubgraphSearcher.h"
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 namespace sgf
 {
@@ -13,34 +24,60 @@ PatternGraphFilter::PatternGraphFilter(std::vector<ColoredGraphPatternResult> li
 {
 }
 
-bool is_check_relevant(const FilterResult& result, const std::unordered_set<uint32_t>& indices_to_filter)
+}  // namespace sgf
+
+namespace
+{
+
+bool is_check_relevant(const sgf::FilterResult& result,
+                       const std::unordered_set<uint32_t>& indices_to_filter)
 {
     return std::all_of(indices_to_filter.cbegin(), indices_to_filter.cend(),
-                       [&result](const uint32_t index) { return result[index]; });
+                       [&result](const uint32_t index)
+                       {
+                           return result[index];
+                       });
 }
 
-FilterResult PatternGraphFilter::filter(const ColoredGraph& graph, bool is_induced, PriorPolicy prior_policy) const
+void log_and_mark_pruned(sgf::FilterResult& result, const uint32_t index,
+                         const sgf::LoggerHandler& logger)
+{
+    if (!result[index])
+    {
+        logger.log(sgf::LogLevel::INFO, "Pruned library graph " + std::to_string(index));
+    }
+    result[index] = true;
+}
+
+void apply_prune(sgf::FilterResult& result, const std::unordered_set<uint32_t>& indices,
+                 const sgf::LoggerHandler& logger)
+{
+    for (const uint32_t index : indices)
+    {
+        log_and_mark_pruned(result, index, logger);
+    }
+}
+
+}  // namespace
+
+namespace sgf
+{
+
+FilterResult PatternGraphFilter::filter(const ColoredGraph& graph, const bool is_induced,
+                                        const PriorPolicy prior_policy) const
 {
     FilterResult result(m_library_cache.size(), false);
-    SubgraphSearcher pattern_searcher(prior_policy, graph.is_directed(), is_induced, nullptr, m_logger);
-    for (size_t library_index = 0; library_index < m_library_cache.size(); ++library_index)
-    {   
-        const ColoredGraphPatternResult& library_result = m_library_cache[library_index];
+    const SubgraphSearcher pattern_searcher(prior_policy, graph.is_directed(), is_induced, nullptr,
+                                            m_logger);
+    for (const auto& library_result : m_library_cache)
+    {
         if (is_check_relevant(result, library_result.second))
+        {
+            if (pattern_searcher.find_all(graph, library_result.first, true) < 1U)
             {
-                if(pattern_searcher.find_all(graph, library_result.first, true) < 1)
-                {
-                    for (const uint32_t index : m_library_cache[library_index].second)
-                    {
-                        if (!result[index])
-                        {
-                            m_logger.log(LogLevel::INFO,
-                                 "Pruned library graph " + std::to_string(index));
-                        }
-                        result[index] = true;
-                    }
-                }
+                apply_prune(result, library_result.second, m_logger);
             }
+        }
     }
     return result;
 }
